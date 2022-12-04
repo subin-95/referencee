6393c574082cdff377a86a5395e0ee2e7c3f6ec2
SUBIN LAL S M <lalsubin3@gmail.com>
2022-12-04 21:58:51 +0530

    i

---
+++ interface/src/pkginterface/Interface.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkginterface;
+
+/**
+ *
+ * @author lalsu
+ */
+public class Interface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        son subin = new son();
+         subin.mother_behaviour();
+        subin.father_behavior();
+        subin.grandma_behavior();
+        subin.grandpa_behaviour();
+    }
+    
+}
